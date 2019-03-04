6a79f77e1fe65d5e3c6021c8a849cc3a6f7db008
Chang <k_chang@tss-hr420-dm14.fc.ca>
2019-03-04 16:08:34 -0500

    control CSS transitions with JS

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@
 	function playDrumKitSound(event) {
 		// debugger;
 		// select the corresponding audio element and make it play
+		// keyCode = keyCode number in console, the data being collected for the data-key
 		let audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
 
 		// debugging / error handling
@@ -11,10 +12,35 @@
 		// ! is not an operator or a bang
 		if (!audio) { return; } //return stops code execution
 
-		// removes delay (makes audio start at 0 on push of key)
+		// removes delay (makes audio restart on push of key)
 		audio.currentTime = 0;
 		audio.play();
+
+		// grab the div and animate it
+		let key = document.querySelector(`div[data-key="${event.keyCode}"]`);
+		key.classList.add('playing');
 	}
+
+	function removePlayingClass(event) {
+		// debugger; // pauses function multiple times for every transition in CSS
+		if (event.propertyName !== "transform") {
+			return;
+		} else {
+			// remove the playing class here from the active div
+			console.log('transition is done');
+
+			// let keyClass = document.querySelectorAll('.key');
+			// keyClass.forEach( playingClass =>
+			// 	playingClass.classList.remove('playing'));
+
+			// shorter way of selecting the div I want
+			event.target.classList.remove('playing');
+		}
+	}
+
+	const keys = Array.from(document.querySelectorAll('.keys'));
+
+	keys.forEach(key => key.addEventListener("transitionend", removePlayingClass));
 
 	// Hey! scrolling uses the window (use in music mixer and final)
 	window.addEventListener('keydown', playDrumKitSound);
